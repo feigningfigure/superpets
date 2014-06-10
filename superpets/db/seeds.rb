43f028e1a6dc1cd5a193e2822c1c5f9fa346f7f6
keyanbagheri <keyanbagheri@gmail.com>
2014-06-10 10:00:16 -0400

    added seed and show

--- superpets/db/seeds.rb
+++ superpets/db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+animals = ["dog", "cat", "mouse", "rat", "parrot", "owl", "ferret", "fish", "hamster", "gerbil", "bunny", "snake", "lizard", "chameleon", "turtle", "starfish", "gecko", "tarantula", "kitten", "puppy", "canary", "horse", "chinchilla"]
+
+superpowers = ["heat-vision", "fire-breath", "slow-motion kung fu", "kung fu grip", "hypnotiq eyes", "poisonous kisses", "super strength", "invisibility", "coding knowledge", "telekenesis", "house brokenness", "super speed", "x-ray vision", "super sneeze", "super photogenisis", "super allergens"]
+
+25.times do
+  # create a chef and shovel them onto temp array
+  new_superpet = Superpet.create({
+    name: Faker::Name.first_name,
+    type: animals.sample,
+    superpower: superpowers.sample,
+    strength: rand(1..10)
+    })
